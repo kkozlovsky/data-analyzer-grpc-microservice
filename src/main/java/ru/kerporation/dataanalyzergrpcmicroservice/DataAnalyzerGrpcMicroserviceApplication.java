4660f23b183d350bc69d541548bc129d9ded742f
kkozlovsky <kkozlovsky@yandex.ru>
2024-03-13 11:15:24 +0300

    Initial commit

---
+++ src/main/java/ru/kerporation/dataanalyzergrpcmicroservice/DataAnalyzerGrpcMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package ru.kerporation.dataanalyzergrpcmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataAnalyzerGrpcMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataAnalyzerGrpcMicroserviceApplication.class, args);
+    }
+
+}
